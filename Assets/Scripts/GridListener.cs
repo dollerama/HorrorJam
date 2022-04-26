7365f93f85c223136b191f8eabb38887589086eb
Graham Sullivan <graham.p.sullivan@gmail.com>
2022-04-26 09:54:20 -0700

    beginning of scare manager

---
+++ Assets/Scripts/GridListener.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridListener : MonoBehaviour
+{
+    private System.Action<GameObject> _triggerAction;
+    public List<GameObject> Contents;
+    private bool _init;
+
+    public void Start()
+    {
+        Contents = new List<GameObject>();
+        _init = false;
+    }
+
+    public void LateUpdate()
+    {
+        if(!_init && this.GetComponent<BoxCollider>())
+        {
+            _init = true;
+            Collider[] colliders = Physics.OverlapBox(this.GetComponent<BoxCollider>().bounds.center, this.GetComponent<BoxCollider>().bounds.extents);
+
+            foreach (Collider c in colliders)
+            {
+                if(c.gameObject && !c.gameObject.CompareTag("Player"))
+                    Contents.Add(c.gameObject);
+            }
+        }
+    }
+
+    public void AddAction(System.Action<GameObject> a)
+    {
+        _triggerAction = a;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player")) _triggerAction.Invoke(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/ScareManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScareManager : MonoBehaviour
+{
+    public GameObject Player;
+    public Vector3 AreaDimensions;
+    public Vector3 Origin;
+    public Vector2 AreaSize;
+    private GameObject[,] Areas;
+
+    public GameObject ActiveGridCell;
+    private GridListener ActiveGridCellListener;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Areas = new GameObject[(int)AreaSize.x,(int)AreaSize.y];
+
+        Vector3 _origin = Origin;
+
+        for(int i=0; i < AreaSize.x; i++)
+        {
+            for(int j=0; j < AreaSize.y; j++)
+            {
+                GameObject tmp = new GameObject($"Area{i}_{j}");
+                tmp.transform.SetParent(this.transform);
+
+                BoxCollider coll = tmp.AddComponent<BoxCollider>();
+                coll.size = AreaDimensions;
+                coll.isTrigger = true;
+
+                //add a listener to each grid cell. this will tell the manager which cell the player is in
+                GridListener gl = tmp.AddComponent<GridListener>();
+                gl.AddAction((tmp) => {
+                    ActiveGridCell = tmp;
+                    ActiveGridCellListener = tmp.GetComponent<GridListener>();
+                });
+
+                tmp.transform.position = _origin;
+                _origin.x += AreaDimensions.x;
+            }
+            _origin.x = Origin.x;
+            _origin.z += AreaDimensions.z;
+        }
+
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+}
